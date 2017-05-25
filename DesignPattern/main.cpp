48d6f9c3d05286c8406a5c09cd37d1941e5c8737
피버스튜디오 <fever@eun.local>
2017-05-25 10:56:08 +0900

    Initial Commit

---
+++ DesignPattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPattern
+//
+//  Created by 피버스튜디오 on 2017. 5. 25..
+//  Copyright © 2017년 DoHyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
