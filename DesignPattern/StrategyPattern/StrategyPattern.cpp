bbd34ab7d467a3fba585ea3c6a831b06a6a009f9
피버스튜디오 <fever@eun.local>
2017-05-25 14:48:19 +0900

    pattern start

---
+++ DesignPattern/StrategyPattern/StrategyPattern.cpp
@@ -0,0 +1,9 @@
+//
+//  StrategyPattern.cpp
+//  DesignPattern
+//
+//  Created by DoHyun on 2017. 5. 25..
+//  Copyright © 2017년 DoHyun. All rights reserved.
+//
+
+#include "StrategyPattern.hpp"
---
+++ DesignPattern/StrategyPattern/StrategyPattern.hpp
@@ -0,0 +1,14 @@
+//
+//  StrategyPattern.hpp
+//  DesignPattern
+//
+//  Created by DoHyun on 2017. 5. 25..
+//  Copyright © 2017년 DoHyun. All rights reserved.
+//
+
+#ifndef StrategyPattern_hpp
+#define StrategyPattern_hpp
+
+#include <stdio.h>
+
+#endif /* StrategyPattern_hpp */
